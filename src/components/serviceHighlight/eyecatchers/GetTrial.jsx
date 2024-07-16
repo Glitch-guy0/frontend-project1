cf6540d84aa30c5debb6bc61f2266a72dfaa594c
Glitch-guy0 <prajwalm9845@gmail.com>
2024-07-16 13:02:14 +0530

    animated logo movement

--- src/components/serviceHighlight/eyecatchers/GetTrial.jsx
+++ src/components/serviceHighlight/eyecatchers/GetTrial.jsx
@@ -1,9 +1,12 @@
+import { useGSAP } from "@gsap/react"
 import trialLogo from "/images/illustrations/trial.svg"
+import { ScrollTrigger } from "gsap/all"
+import gsap from "gsap"
 
 
 
 
-
+// gsap.registerPlugin(ScrollTrigger);
 
 
 
@@ -11,6 +14,22 @@ import trialLogo from "/images/illustrations/trial.svg"
 
 
 export default function GetTrial(){
+
+    useGSAP(()=>{
+        gsap.fromTo("#trialLogo", {
+            y: 50,
+            
+        }, {
+            y: -50,
+            scrollTrigger: {
+                trigger: "#trialLogo",
+                scroller: "body",
+                scrub: 1
+            }
+        })
+    })
+
+
     return(
         <div className=" bg-slate-200 rounded-3xl flex p-12 relative mx-12">
             <div className="w-2/5">
@@ -18,7 +37,7 @@ export default function GetTrial(){
                 <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique neque perferendis labore ad, eveniet ullam. Dolor, gnam suscipit porro nesciunt ex!</p>
                 <button className="mt-6 bg-black text-white px-8 py-4 rounded-xl font-semibold">Get your free proposal</button>
             </div>
-            <img src={trialLogo} alt=""  className="absolute right-40 top-[-60px]"/>
+            <img id="trialLogo" src={trialLogo} alt=""  className="absolute right-40 top-[-60px]"/>
         </div>
     )
 }
